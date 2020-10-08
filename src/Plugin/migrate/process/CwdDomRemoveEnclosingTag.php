b21b4440b5a584cbfb99c6bdfcbae41ba581a8f5
Alison Jo <2660348+alisonjo315@users.noreply.github.com>
2020-10-08 19:24:14 -0400

    code comment

--- src/Plugin/migrate/process/CwdDomRemoveEnclosingTag.php
+++ src/Plugin/migrate/process/CwdDomRemoveEnclosingTag.php
@@ -13,6 +13,10 @@ use Drupal\migrate_plus\Plugin\migrate\process\DomProcessBase;
  * Context is optional (entire field value by default), xpath is required
  * and can support removing multiple nodes in the same process run.
  *
+ * NOTE: I *think* we didn't end up using this... I think we ended up
+ * putting everything into ./CwdRichTextDomProcess.php, and there's a
+ * (simpler) function in ../../../Utility.php: removeMatchedEnclosingNode()
+ *
  * Example:
  *
  * @code
